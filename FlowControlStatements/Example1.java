cb67e59a9567eb89efaf3bfc95257a52d9d16fed
Revanth <revanthnagidi1@gmail.com>
2025-07-05 12:20:00 +0530

    till example3

---
+++ FlowControlStatements/Example1.java
@@ -0,0 +1,29 @@
+public class Example1{
+    public static void main(String[] args){
+
+
+        //a
+
+        // int a = 15;
+        // if(a<0){
+        //     System.out.println("Negative");
+        // }
+        // if(a>0){
+        //     System.out.println("Positive");
+        // }
+        // if(a==0){
+        //     System.out.println("Zero");
+        // }
+
+
+        // b
+        int a = 12;
+        int b = 22;
+        if(a%10 == b%10){
+            System.out.println("True");
+        }
+        else{
+            System.out.println("False");
+        }
+    }
+}
