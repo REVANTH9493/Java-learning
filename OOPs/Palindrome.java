eed8c7328143f96efb948afd48cf6e07a6245a09
Revanth <revanthnagidi1@gmail.com>
2025-08-06 07:25:20 +0530

    Palindrome

---
+++ OOPs/Palindrome.java
@@ -0,0 +1,11 @@
+public class Palindrome {
+    public static void main(String[] args) {
+        String a = "121";
+        String b = "";
+        for(int i=a.length()-1; i>=0; i--){
+            b = b+a.charAt(i);
+        }
+        if(a.equals(b)) System.out.println("Palindrome");
+        else System.out.println("Not a palindrome");
+    }
+}
